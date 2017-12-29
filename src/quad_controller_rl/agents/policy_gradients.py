53011316284b6e3f377c8897f19d1cad3f3ecbcf
Shubham Chandel <shubham.zeez@gmail.com>
2017-12-30 00:15:00 +0530

    Agent: DDPG p-holders to class level

--- src/quad_controller_rl/agents/policy_gradients.py
+++ src/quad_controller_rl/agents/policy_gradients.py
@@ -60,62 +60,62 @@ class DDPG(BaseAgent):
 		tf.reset_default_graph()
 
 		# placeholders
-		state_ph = tf.placeholder(dtype=tf.float32, shape=[None,state_dim])
-		action_ph = tf.placeholder(dtype=tf.float32, shape=[None,self.action_dim])
-		reward_ph = tf.placeholder(dtype=tf.float32, shape=[None])
-		next_state_ph = tf.placeholder(dtype=tf.float32, shape=[None,state_dim])
-		is_not_terminal_ph = tf.placeholder(dtype=tf.float32, shape=[None]) # indicators (go into target computation)
-		is_training_ph = tf.placeholder(dtype=tf.bool, shape=()) # for dropout
+		self.state_ph = tf.placeholder(dtype=tf.float32, shape=[None,state_dim])
+		self.action_ph = tf.placeholder(dtype=tf.float32, shape=[None,self.action_dim])
+		self.reward_ph = tf.placeholder(dtype=tf.float32, shape=[None])
+		self.next_state_ph = tf.placeholder(dtype=tf.float32, shape=[None,state_dim])
+		self.is_not_terminal_ph = tf.placeholder(dtype=tf.float32, shape=[None]) # indicators (go into target computation)
+		self.is_training_ph = tf.placeholder(dtype=tf.bool, shape=()) # for dropout
 
 		# episode counter
 		episodes = tf.Variable(0.0, trainable=False, name='episodes')
-		episode_inc_op = episodes.assign_add(1)
+		self.episode_inc_op = episodes.assign_add(1)
 
 		# will use this to initialize both the actor network its slowly-changing target network with same structure
 		def generate_actor_network(s, trainable, reuse):
 			hidden = tf.layers.dense(s, h1_actor, activation = tf.nn.relu, trainable = trainable, name = 'dense', reuse = reuse)
-			hidden_drop = tf.layers.dropout(hidden, rate = dropout_actor, training = trainable & is_training_ph)
+			hidden_drop = tf.layers.dropout(hidden, rate = dropout_actor, training = trainable & self.is_training_ph)
 			hidden_2 = tf.layers.dense(hidden_drop, h2_actor, activation = tf.nn.relu, trainable = trainable, name = 'dense_1', reuse = reuse)
-			hidden_drop_2 = tf.layers.dropout(hidden_2, rate = dropout_actor, training = trainable & is_training_ph)
+			hidden_drop_2 = tf.layers.dropout(hidden_2, rate = dropout_actor, training = trainable & self.is_training_ph)
 			hidden_3 = tf.layers.dense(hidden_drop_2, h3_actor, activation = tf.nn.relu, trainable = trainable, name = 'dense_2', reuse = reuse)
-			hidden_drop_3 = tf.layers.dropout(hidden_3, rate = dropout_actor, training = trainable & is_training_ph)
+			hidden_drop_3 = tf.layers.dropout(hidden_3, rate = dropout_actor, training = trainable & self.is_training_ph)
 			actions_unscaled = tf.layers.dense(hidden_drop_3, self.action_dim, trainable = trainable, name = 'dense_3', reuse = reuse)
 			actions = self.task.action_space.low + tf.nn.sigmoid(actions_unscaled)*(self.task.action_space.high - self.task.action_space.low) # bound the actions to the valid range
 			return actions
 
 		# actor network
 		with tf.variable_scope('actor'):
-			# Policy's outputted action for each state_ph (for generating actions and training the critic)
-			self.actions = generate_actor_network(state_ph, trainable = True, reuse = False)
+			# Policy's outputted action for each self.state_ph (for generating actions and training the critic)
+			self.actions = generate_actor_network(self.state_ph, trainable = True, reuse = False)
 
 		# slow target actor network
 		with tf.variable_scope('slow_target_actor', reuse=False):
-			# Slow target policy's outputted action for each next_state_ph (for training the critic)
+			# Slow target policy's outputted action for each self.next_state_ph (for training the critic)
 			# use stop_gradient to treat the output values as constant targets when doing backprop
-			slow_target_next_actions = tf.stop_gradient(generate_actor_network(next_state_ph, trainable = False, reuse = False))
+			slow_target_next_actions = tf.stop_gradient(generate_actor_network(self.next_state_ph, trainable = False, reuse = False))
 
 		# will use this to initialize both the critic network its slowly-changing target network with same structure
 		def generate_critic_network(s, a, trainable, reuse):
 			state_action = tf.concat([s, a], axis=1)
 			hidden = tf.layers.dense(state_action, h1_critic, activation = tf.nn.relu, trainable = trainable, name = 'dense', reuse = reuse)
-			hidden_drop = tf.layers.dropout(hidden, rate = dropout_critic, training = trainable & is_training_ph)
+			hidden_drop = tf.layers.dropout(hidden, rate = dropout_critic, training = trainable & self.is_training_ph)
 			hidden_2 = tf.layers.dense(hidden_drop, h2_critic, activation = tf.nn.relu, trainable = trainable, name = 'dense_1', reuse = reuse)
-			hidden_drop_2 = tf.layers.dropout(hidden_2, rate = dropout_critic, training = trainable & is_training_ph)
+			hidden_drop_2 = tf.layers.dropout(hidden_2, rate = dropout_critic, training = trainable & self.is_training_ph)
 			hidden_3 = tf.layers.dense(hidden_drop_2, h3_critic, activation = tf.nn.relu, trainable = trainable, name = 'dense_2', reuse = reuse)
-			hidden_drop_3 = tf.layers.dropout(hidden_3, rate = dropout_critic, training = trainable & is_training_ph)
+			hidden_drop_3 = tf.layers.dropout(hidden_3, rate = dropout_critic, training = trainable & self.is_training_ph)
 			q_values = tf.layers.dense(hidden_drop_3, 1, trainable = trainable, name = 'dense_3', reuse = reuse)
 			return q_values
 
 		with tf.variable_scope('critic') as scope:
-			# Critic applied to state_ph and a given action (for training critic)
-			q_values_of_given_actions = generate_critic_network(state_ph, action_ph, trainable = True, reuse = False)
-			# Critic applied to state_ph and the current policy's outputted actions for state_ph (for training actor via deterministic policy gradient)
-			q_values_of_suggested_actions = generate_critic_network(state_ph, actions, trainable = True, reuse = True)
+			# Critic applied to self.state_ph and a given action (for training critic)
+			q_values_of_given_actions = generate_critic_network(self.state_ph, self.action_ph, trainable = True, reuse = False)
+			# Critic applied to self.state_ph and the current policy's outputted actions for self.state_ph (for training actor via deterministic policy gradient)
+			q_values_of_suggested_actions = generate_critic_network(self.state_ph, actions, trainable = True, reuse = True)
 
 		# slow target critic network
 		with tf.variable_scope('slow_target_critic', reuse=False):
-			# Slow target critic applied to slow target actor's outputted actions for next_state_ph (for training critic)
-			slow_q_values_next = tf.stop_gradient(generate_critic_network(next_state_ph, slow_target_next_actions, trainable = False, reuse = False))
+			# Slow target critic applied to slow target actor's outputted actions for self.next_state_ph (for training critic)
+			slow_q_values_next = tf.stop_gradient(generate_critic_network(self.next_state_ph, slow_target_next_actions, trainable = False, reuse = False))
 
 		# isolate vars for each network
 		actor_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='actor')
@@ -133,12 +133,12 @@ class DDPG(BaseAgent):
 			update_slow_target_critic_op = slow_target_var.assign(tau*critic_vars[i]+(1-tau)*slow_target_var)
 			update_slow_target_ops.append(update_slow_target_critic_op)
 
-		update_slow_targets_op = tf.group(*update_slow_target_ops, name='update_slow_targets')
+		self.update_slow_targets_op = tf.group(*update_slow_target_ops, name='update_slow_targets')
 
 		# One step TD targets y_i for (s,a) from experience replay
 		# = r_i + gamma*Q_slow(s',mu_slow(s')) if s' is not terminal
 		# = r_i if s' terminal
-		targets = tf.expand_dims(reward_ph, 1) + tf.expand_dims(is_not_terminal_ph, 1) * gamma * slow_q_values_next
+		targets = tf.expand_dims(self.reward_ph, 1) + tf.expand_dims(self.is_not_terminal_ph, 1) * gamma * slow_q_values_next
 
 		# 1-step temporal difference errors
 		td_errors = targets - q_values_of_given_actions
@@ -203,8 +203,8 @@ class DDPG(BaseAgent):
 	def step(self, observation, reward, done):
 		# choose action based on deterministic policy
 		action_for_state, = self.sess.run(self.actions, feed_dict = {
-			state_ph: observation, 
-			is_training_ph: False
+			self.state_ph: observation, 
+			self.is_training_ph: False
 		})
 
 		# add temporally-correlated exploration noise to action (using an Ornstein-Uhlenbeck process)
@@ -232,23 +232,30 @@ class DDPG(BaseAgent):
 			# update the critic and actor params using mean-square value error and deterministic policy gradient, respectively
 			_, _ = self.sess.run([self.critic_train_op, self.actor_train_op], 
 				feed_dict = {
-					state_ph: np.asarray([elem[0] for elem in minibatch]),
-					action_ph: np.asarray([elem[1] for elem in minibatch]),
-					reward_ph: np.asarray([elem[2] for elem in minibatch]),
-					next_state_ph: np.asarray([elem[3] for elem in minibatch]),
-					is_not_terminal_ph: np.asarray([elem[4] for elem in minibatch]),
-					is_training_ph: True})
+					self.state_ph: np.asarray([elem[0] for elem in minibatch]),
+					self.action_ph: np.asarray([elem[1] for elem in minibatch]),
+					self.reward_ph: np.asarray([elem[2] for elem in minibatch]),
+					self.next_state_ph: np.asarray([elem[3] for elem in minibatch]),
+					self.is_not_terminal_ph: np.asarray([elem[4] for elem in minibatch]),
+					self.is_training_ph: True})
 
 			# update slow actor and critic targets towards current actor and critic
-			_ = self.sess.run(update_slow_targets_op)
+			_ = self.sess.run(self.update_slow_targets_op)
+
+
+		self.last_observation = observation
+		self.last_action = action
+		self.total_steps += 1
+		self.steps_in_ep += 1
+		
 
 		# Learn, if at end of episode
 		if done:
-			self.learn()
+			_ = sess.run(self.episode_inc_op)
 			self.reset_episode_vars()
+			print('Episode %2i, Reward: %7.3f, Steps: %i, Final noise scale: %7.3f'%(self.ep, self.total_reward, self.steps_in_ep, self.noise_scale))
 
-		self.last_state = state
-		self.last_action = action
 		return action
+
 
 
